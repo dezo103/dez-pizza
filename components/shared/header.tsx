b06673f9e42822051c5fde936f7a05dbef1cdc0a
Alexander Rudyk <dezo103@gmail.com>
2025-04-17 20:33:44 +0300

    feat: change name

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -19,7 +19,7 @@ export const Header: React.FC<Props> = ({ className }) => {
           <div className="flex items-center gap-4">
             <Image src="/logo.png" alt="Logo" width={35} height={35} />
             <div>
-              <h1 className="text-2xl uppercase font-black">Molo Pizza</h1>
+              <h1 className="text-2xl uppercase font-black">Dez-Pizza</h1>
               <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
             </div>
           </div>
