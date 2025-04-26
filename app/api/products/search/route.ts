8d24551349b3beb0b36dba2d7328c5603c61a973
Rudyk Alexander <dezo103@gmail.com>
2025-04-26 11:10:02 +0300

    feat: add products search

---
+++ app/api/products/search/route.ts
@@ -0,0 +1,8 @@
+import { prisma } from '@/prisma/prisma-client';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  console.log(req);
+
+  return NextResponse.json(req);
+}
