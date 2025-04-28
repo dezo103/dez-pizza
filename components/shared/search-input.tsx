6e98a0bf50f05872debfc2c52843fddc2d58595b
Rudyk Alexander <dezo103@gmail.com>
2025-04-28 23:36:56 +0300

    feat: add search input styles

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import React from 'react';
+import { cn } from '@/lib/utils';
 
 interface Props {
   className?: string;
 }
 
-export const SearchInput: React.FC<Props> = () => {
-  return <>search input</>;
+export const SearchInput: React.FC<Props> = ({className}) => {
+  return <div className={cn('flex rounded-2xl flex-1 justify-between relative h-11 z-30', className)}>search input</div>;
 };
