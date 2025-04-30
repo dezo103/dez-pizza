98469da9b3df7a451e28fee5425354dcbd4e2046
Rudyk Alexander <dezo103@gmail.com>
2025-04-30 23:12:15 +0300

    feat: add empty hook filterIngredients

---
+++ hooks/useFilterIngredients.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type ReturnProps = {
+  className?: string;
+};
+
+export const UseFilterIngredients: React.FC<ReturnProps> = ({ className }) => {
+  return <div className={className}>there will be body of hooks</div>;
+};
